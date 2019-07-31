18e971d47b24d48dda6437fdfc209e884db745db
Sayne Rojas <saynegrojas@yahoo.com>
2019-07-30 19:20:35 -0700

    sequelize connection

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+// //Dependencies
+// const Sequelize = require('sequelize');
+
+// //Create mysql connection using sequelize
+// const sequelize = new Sequelize('drink_properly_db', 'root', 'Greg@rojas00',{
+//     host: 'localhost',
+//     dialect: 'mysql',
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         idle: 10000
+//     }
+// });
+
+// //export connection
+// module.exports = sequelize;

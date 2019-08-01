d1c438ed547ecbdfb154f365b80d8b401da23ddc
Sayne Rojas <saynegrojas@yahoo.com>
2019-07-31 20:26:28 -0700

    Added minor functionality to js file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
 module.exports = function(app){
-    //html index route
+
+    //HTML index route
     app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });

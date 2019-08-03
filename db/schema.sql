47f72b3ae8c680d399d069f8309855b124a9a08f
ChrisA <cteg516@gmail.com>
2019-08-02 21:07:09 -0800

    t

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,5 @@
 CREATE TABLE localhappyhour(
-<<<<<<< HEAD
-id INT(11) NOT NULL AUTO_INCREMENT,
-=======
 id INT (11) NOT NULL AUTO_INCREMENT,
->>>>>>> 6b37ab82fad8ed3cbdf895a35a9aab2e09d3e9ec
 ZIPCODE CHAR(6),
 restaurant VARCHAR(100) NOT NULL,
 drink VARCHAR(100),

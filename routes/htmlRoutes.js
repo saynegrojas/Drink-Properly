cb805d8f2b68ae0507f20bb6c05168e444f94996
Sayne Rojas <saynegrojas@yahoo.com>
2019-08-04 01:03:46 -0700

    added a menu card on html, update route, single category, another route to html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,19 @@
+//================================================================
+//HTML Route
+//NPM Packages
+//================================================================
 const htmlRoute = require('../models');
+const path = require('path');
 
+//Routes
 module.exports = function(app) {
 
-    //HTML index route
+    //HTML Index Route loads home
     app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
+    //Index is (convention) /index as a route
+    app.get('/index', (req, res) => {
+        res.sendFile(path.join(__dirname,'../public/index.html' ))
+    })
 };

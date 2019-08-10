65c16cf9bc3b8054eb0f74acb5a0df1a38c1a4eb
vikctoria stephenson <vicstephjackson@gmail.com>
2019-08-10 11:38:52 -0700

    updated

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const express = require('express');
 const app = express();
 
 //Port
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 //connect with models
 const db = require('./models');
